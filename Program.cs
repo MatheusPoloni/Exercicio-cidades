727b3c76a5697bff6cbc4c3d8be6c83d21de69f9
matheus <machadomatheus0000@gmail.com>
2022-03-01 17:22:13 -0300

    Matheus Guilherme

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Mongaguá!");
+Console.WriteLine("Itanhaém!");
+Console.WriteLine("Peruíbe!");
+Console.WriteLine("Praia Grande!");
+Console.WriteLine("Guarujá!");
+Console.WriteLine("Santos!");
+Console.WriteLine("São Vicente!");
